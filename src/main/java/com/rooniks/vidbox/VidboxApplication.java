4d3a2648bfdbda238e5a9b01ecbea3c24233f01a
Rooniks <me.rooniks@gmail.com>
2020-06-17 00:20:59 +0530

    Initial commit

---
+++ src/main/java/com/rooniks/vidbox/VidboxApplication.java
@@ -0,0 +1,11 @@
+package com.rooniks.vidbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidboxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VidboxApplication.class, args);
+    }
+}
---
+++ src/test/java/com/rooniks/vidbox/VidboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rooniks.vidbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
